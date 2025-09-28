6d98db48fbacc9883d748fc84d6dc1880f86ab08
satwashi <satwashi@proton.me>
2025-09-28 20:33:29 +0300

    let's goooo

--- app/(main)/wallet/_cmp/realtime-wallets.tsx
+++ app/(main)/wallet/_cmp/realtime-wallets.tsx
@@ -1,6 +1,7 @@
 import { useUserWallets } from "@/queries/wallets/use-user-wallets";
 import { WalletCard } from "./wallet-card";
 import { WalletCardSkeleton } from "./skeletons/wallets-card-skeleton";
+import WalletCardWithProgress from "./wallet-creation/wallet-card-with-progress";
 
 export default function RealtimeWallets({ userId }: { userId: string }) {
   const { wallets, isLoading, error } = useUserWallets(userId);
@@ -15,9 +16,19 @@ export default function RealtimeWallets({ userId }: { userId: string }) {
     );
 
   if (error) return <>Error</>;
-
   if (!wallets || wallets.length === 0) {
-    /* ... your empty state ... */
+    return (
+      <div className="min-h-[60vh] md:min-h-[70vh] flex items-center justify-center">
+        <div className="w-full max-w-md">
+          <WalletCardWithProgress>
+            <div className="mb-6 p-4 rounded-xl ">
+              <h4 className=" font-semibold mb-2">Trading Wallet</h4>
+              <p className="text-sm">Enhanced features with exchange support</p>
+            </div>
+          </WalletCardWithProgress>
+        </div>
+      </div>
+    );
   }
 
   return (
