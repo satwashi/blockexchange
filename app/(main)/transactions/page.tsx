c1374d9571dc033e4f44cd84f5c7341ac2cdd980
satwashi <satwashi@proton.me>
2025-12-11 11:21:54 +0300

    let's goooo baby

--- app/(main)/transactions/page.tsx
+++ app/(main)/transactions/page.tsx
@@ -1,7 +1,5 @@
-import { getServerSession } from "@/server/user/users";
 import Main from "./_cmp/main";
 
-export default async function TransactionsPage() {
-  const { id: user_id } = await getServerSession();
-  return <Main user_id={user_id} />;
+export default function TransactionsPage() {
+  return <Main />;
 }
